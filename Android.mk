d44ac8a07417df47633a795330becf2a43bd6d26
lozohcum <lozohcum@gmail.com>
2013-12-29 12:39:43 +0100

    Added missing memtrack module

--- Android.mk
+++ Android.mk
@@ -5,6 +5,7 @@ ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),legacy)
 
 display-hals := libgralloc libgenlock libcopybit
 display-hals += libhwcomposer liboverlay libqdutils libtilerenderer
+display-hals += libmemtrack
 
 include $(call all-named-subdir-makefiles,$(display-hals))
 
